98aa234b314e3dcbff5bf1d680be7ad5a2a41914
javython999 <infinity2544@gmail.com>
2025-08-20 20:54:36 +0900

    Integer.MIN_VALUE

---
+++ src/Main029.java
@@ -0,0 +1,9 @@
+public class Main029 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+
+        System.out.println(Math.absExact(Integer.MIN_VALUE));
+    }
+}
