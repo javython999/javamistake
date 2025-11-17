59766f4efadff7a75d81d8778ba0ffdc57a7dbab
javython999 <infinity2544@gmail.com>
2025-11-17 20:15:40 +0900

    문자열을 불리언으로 잘못 변환

---
+++ src/Main089.java
@@ -0,0 +1,10 @@
+public class Main089 {
+
+    public static void main(String[] args) {
+        System.out.println(Boolean.parseBoolean(" true "));
+        System.out.println(Boolean.parseBoolean("Yes"));
+        System.out.println(Boolean.parseBoolean("Y"));
+        System.out.println(Boolean.parseBoolean("T"));
+        System.out.println(Boolean.parseBoolean(null));
+    }
+}
